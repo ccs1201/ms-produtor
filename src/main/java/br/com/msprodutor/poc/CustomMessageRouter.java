aba0f601163fc0d7c99e91b5d8226386b580094f
Cleber Souza <cleber.constante@totvs.com.br>
2025-05-26 11:28:33 -0300

    # testes

--- src/main/java/br/com/msprodutor/poc/CustomMessageRouter.java
+++ src/main/java/br/com/msprodutor/poc/CustomMessageRouter.java
@@ -1,16 +1,22 @@
 package br.com.msprodutor.poc;
 
 import br.com.ccs.messagedispatcher.router.MessageRouter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 //@Component
 //@Primary
+@Slf4j
 public class CustomMessageRouter implements MessageRouter {
 
     @Override
-    public Object routeMessage(Message message) {
+    public Object routeMessage(Object message) {
+        var messageBody = ((Message) message).getBody();
+        log.info("CustomMessageRouter: {}", Arrays.toString(messageBody));
         return null;
     }
 }
