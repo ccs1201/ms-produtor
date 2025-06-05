c385772896f26fa4a17b624f4dbf837d79007d12
Cleber Souza <cleber.constante@totvs.com.br>
2025-06-05 17:30:11 -0300

    feat: melhora dos logs de exception quando em debug mode e dos headers de exception quando enviado para dlq.
    Trata a presença dos headers obrigatórios no AnnotatedMessageRouter.
    
    Melhora a descrição dos headers.
    
    Altera o enum MessageType e Headers para HandlerType para ficar mais conciso.

--- src/main/java/br/com/msprodutor/commandrunner/AppCMD.java
+++ src/main/java/br/com/msprodutor/commandrunner/AppCMD.java
@@ -9,22 +9,39 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @RequiredArgsConstructor
-@Component
+//@Component
 public class AppCMD {
 
     private final MessagePublisher messagePublisher;
 
     @Bean
     CommandLineRunner run() {
-        log.info("Criando mensagens....");
+        log.info("Enviando mensagens....");
         return args -> {
-            for (int i = 0; i < 1000; i++) {
-                messagePublisher.sendNotification(MsConsumidor.MS_CONSUMIDOR_RK,
-                        new NotificationController.SucessoRecord(i));
-                log.info("Mensagem {} enviada.", i);
+
+            var executor = Executors.newFixedThreadPool(100, Thread.ofVirtual().factory());
+            var qtdMsgs = 100_000;
+
+            AtomicInteger counter = new AtomicInteger(0);
+            List<CompletableFuture> futures = new ArrayList<>(qtdMsgs);
+            for (int i = 0; i < qtdMsgs; i++) {
+                futures.add(CompletableFuture.runAsync(() -> {
+                    var n = counter.incrementAndGet();
+                    messagePublisher.sendNotification(MsConsumidor.MS_CONSUMIDOR_RK,
+                            new NotificationController.SucessoRecord(n));
+                    log.info("Mensagem {} enviada.", n);
+                }, executor));
             }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+            log.info("Mensagens enviadas.");
         };
     }
 }
