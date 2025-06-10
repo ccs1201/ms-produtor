662e82a2240c90d720e3d12be7e9ce5794e62db1
Cleber Souza <cleber.constante@totvs.com.br>
2025-06-10 19:08:27 -0300

    testes

--- src/main/java/br/com/msprodutor/controller/EventController.java
+++ src/main/java/br/com/msprodutor/controller/EventController.java
@@ -4,11 +4,13 @@ import br.com.messagedispatcher.publisher.MessagePublisher;
 import br.com.msprodutor.constants.MsProdutorConstants.MsConsumidor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("ClassEscapesDefinedScope")
 @RestController
 @RequestMapping("/event")
 @RequiredArgsConstructor
@@ -17,13 +19,21 @@ public class EventController {
 
     private final MessagePublisher publisher;
 
-    @SuppressWarnings("ClassEscapesDefinedScope")
     @PostMapping
     public void sendEvent(@RequestBody EventPayload payload) {
         publisher.sendEvent(MsConsumidor.MS_CONSUMIDOR_EVENT_QUEUE, payload);
         log.info("Método sendEvent | Mensagem enviada: {}", payload);
     }
 
+    @PatchMapping
+    public void sendEventErro() {
+        publisher.sendEvent(MsConsumidor.MS_CONSUMIDOR_EVENT_QUEUE, new EventErrorPayload());
+        log.info("Método sendEventErro | Mensagem enviada:");
+    }
+
     private record EventPayload(Integer idade) {
+    }
+
+    private record EventErrorPayload() {
     }
 }
