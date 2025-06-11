ea747307423b50a2a0cd6c5705d5f5693019e9b9
Cleber.Ssouza <ccs1201@gmail.com>
2025-06-11 19:58:55 -0300

    testes

--- src/main/java/br/com/msprodutor/controller/NotificationController.java
+++ src/main/java/br/com/msprodutor/controller/NotificationController.java
@@ -3,20 +3,15 @@ package br.com.msprodutor.controller;
 import br.com.messagedispatcher.annotation.MessageListener;
 import br.com.messagedispatcher.annotation.Notification;
 import br.com.messagedispatcher.publisher.MessagePublisher;
-import br.com.msprodutor.constants.MsProdutorConstants.MsConsumidor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static br.com.msprodutor.constants.MsProdutorConstants.MsConsumidor.*;
+import static br.com.msprodutor.constants.MsProdutorConstants.MsConsumidor.MS_CONSUMIDOR_QUEUE;
 
 @RestController
 @RequestMapping("/")
